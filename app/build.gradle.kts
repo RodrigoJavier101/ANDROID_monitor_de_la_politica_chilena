5f19632997457c6a40226537da81cc19ec35417a
rj <rj@gigioberto.com>
2025-03-14 13:14:16 -0300

    project running, dependencies, and other configurations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,15 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    //    added
+    alias(libs.plugins.android.dagger.hilt)
+    id("kotlin-android")
+    id("kotlin-kapt")
 }
 
 android {
     namespace = "cl.rodrigojavier101.monitordelapoliticachilena"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "cl.rodrigojavier101.monitordelapoliticachilena"
@@ -56,4 +60,84 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    // ###########
+    implementation(libs.androidx.appcompat)
+
+    //*splashscreen*//*
+    implementation(libs.androidx.splash)
+    //*navigation NO CAMBIAR LA VERSION "2.6.0" **************//*
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.fragmentktx)
+    implementation(libs.androidx.legacy)
+    //*picasso*//*
+    implementation(libs.androidx.picasso)
+    // constraint layout
+    implementation(libs.androidx.constraintlayout)
+    //    coroutines
+    implementation(libs.androidx.coroutines)
+    implementation(libs.androidx.coroutinesandroid)
+    //   coroutine lifecycle scopes
+    implementation(libs.androidx.activityktx)
+    implementation(libs.androidx.lifecyclesextensions)
+    implementation(libs.androidx.lifecycle)
+    // KTX - Viewmodel Y Livedata NO ACTUALIZAR: 2.5.1
+    implementation(libs.androidx.lifecycle.livedata)
+    //    data store
+    implementation(libs.androidx.datastore)
+    //    retrofit
+    implementation(libs.androidx.retrofit)
+    implementation(libs.androidx.retrofitgson)
+
+    implementation(libs.googlegson)
+
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.interceptor)
+    //*implementacion para el carrousel*//*
+    // Optional: Circle Indicator (To fix the xml preview "Missing classes" error)
+    implementation(libs.circleindicator)
+    implementation(libs.carrousel)
+    //*material design*//*
+    implementation(libs.materialgoogle)
+    implementation(libs.jsoup)
+    //Room
+    implementation(libs.room)
+    // kapt genera el codigo para la base de datos
+    kapt(libs.roomkaptcompiler)
+// migrating to ksp
+    /* <><><><> *//* <><><><> *//* <><><><> *//* <><><><> */
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+// TEST unitarios
+    testImplementation(libs.junit)
+
+//   added tests MOKK y mockito
+    testImplementation(libs.mokk)
+    testImplementation(libs.mockitocore)
+    testImplementation(libs.mockitoinline)
+
+// TEST
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    //   dagger hilt
+    implementation(libs.dagger.google)
+    implementation(libs.androidx.hilt.navigation.compose.v120)
+    annotationProcessor(libs.androidx.google.compiler)
+    kapt(libs.androidx.google.dagger.kapt.android)
+    kapt(libs.androidx.google.compiler)
+    kapt(libs.androidx.hilt.compiler.v120)
+
 }
+kapt { correctErrorTypes = true }

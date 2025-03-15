ecb07dbb108cdc26072be8c96b81a5fdaa67f705
rj <rj@gigioberto.com>
2025-03-14 21:25:47 -0300

    solved itmust change the theme values > theme and theme in the manifest

--- app/src/main/java/cl/rodrigojavier101/monitordelapoliticachilena/ui/activities/MainActivity.kt
+++ app/src/main/java/cl/rodrigojavier101/monitordelapoliticachilena/ui/activities/MainActivity.kt
@@ -1,50 +1,22 @@
 package cl.rodrigojavier101.monitordelapoliticachilena.ui.activities
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.ui.tooling.preview.Preview
+import cl.rodrigojavier101.monitordelapoliticachilena.databinding.ActivityMainBinding
 import cl.rodrigojavier101.monitordelapoliticachilena.ui.theme.MonitorDeLaPoliticaChilenaTheme
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.HiltAndroidApp
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            MonitorDeLaPoliticaChilenaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "monitor de la politica chilena",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MonitorDeLaPoliticaChilenaTheme {
-        Greeting("Android")
-    }
-}
